5f9bc6db9dfc9da6a3112f70738d544a5e3fdfbd
Patrick Lee <patricklee1138@live.com>
2021-08-05 00:28:10 -0700

    add state-based button visibility

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,27 +1,36 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonItem, IonButton } from '@ionic/react'
-import { useEffect, useCallback, useMemo, useState } from 'react'
+import { IonButton } from '@ionic/react'
+import { useEffect, useCallback, useState } from 'react'
 import { msToClockString } from '../utilities/printTime'
 
 const Timer: React.FC = () => {
   const [ms, setMs] = useState(0) 
   const [isRunning, setIsRunning] = useState(false) // Defines isRunning as a boolean
+  const [buttonText, setButtonText] = useState(["Start", "Reset"])
 
-  const tick = useCallback(async () => {setMs(ms + 10)}, [ms])
+  const tick = useCallback(() => {setMs(ms + 10)}, [ms])
 
   useEffect(() => {
     const timerId = isRunning ? setInterval(tick, 10) : undefined  
     return () => timerId ? clearInterval(timerId) : undefined
   }, [isRunning, tick])
 
+  const isRunningToggle = () => (
+    isRunning
+    ? (setIsRunning(false), setButtonText(["Start", "Reset"]))
+    : (setIsRunning(true), setButtonText(["Pause", "Lap"]))
+  )
+
+  const resetOrLap = () => (
+    isRunning
+    ? setMs(ms)
+    : setMs(0)
+  )
 
   return (
     <div>
       <h1>{msToClockString(ms)}</h1>
-      <IonButton onClick={()=> setIsRunning(true)}>Start</IonButton>
-      <IonButton>Reset</IonButton>
-      <br></br>
-      <IonButton>Lap</IonButton>
-      <IonButton onClick={()=> setIsRunning(false)}>Pause</IonButton>
+      <IonButton onClick={isRunningToggle}>{buttonText[0]}</IonButton>
+      <IonButton onClick={resetOrLap}>{buttonText[1]}</IonButton>
     </div>
   )
 }
--- src/utilities/printTime.ts
+++ src/utilities/printTime.ts
@@ -4,17 +4,9 @@ export const msToClockString = (ms: number):string => {
   const seconds = Math.floor(ms / 1000) % 60
   const minutes = Math.floor(ms / 60000) % 60
 
-  const printHundredths = (hundredths < 10)
-    ? `0${hundredths}` 
-    : hundredths
-
-  const printSeconds = (seconds < 10)
-    ? `0${seconds}` 
-    : seconds
-  
   function annexZero(time: number) {
     return (time < 10) ? `0${time}` : time
   }
 
-  return `${minutes}:${annexZero(seconds)}:${annexZero(hundredths)}`
+  return `${annexZero(minutes)}:${annexZero(seconds)}:${annexZero(hundredths)}`
 }
